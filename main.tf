fa8e5f8315b84bc868bd5913414e6deea6e4c39b
Andy Walker <andrewwa@kainos.com>
2018-07-05 16:39:24 +0100

    ROC-3821 Action group template (#1)
    ROC-3821 Update action group template to support just one email and no sms or webhooks

--- main.tf
+++ main.tf
@@ -10,11 +10,9 @@ resource "azurerm_template_deployment" "action-group" {
 
   parameters = {
     location                           = "${var.location}"
-    env                                = "${var.env}"
-    alertName                          = "${var.group_name}"
-    alertShortName                     = "${var.group_short_name}"
-    alertEmailReceivers                = "${var.alert_email_receivers}"
-    alertSmsReceivers                  = "${var.alert_sms_receivers}"
-    alertWebhookReceivers              = "${var.alert_webhook_receivers}"
+    alertName                          = "${var.alert_name}"
+    alertShortName                     = "${var.alert_short_name}"
+    emailReceiverName                  = "${var.email_receiver_name}"
+    emailReceiverAddress               = "${var.email_receiver_address}"
   }
 }
--- variables.tf
+++ variables.tf
@@ -18,56 +18,18 @@ variable "group_name" {
   type = "string"
 }
 
-variable "group_short_name" {
+variable "alert_name" {
   type = "string"
 }
 
-variable "alert_email_receivers" {
-  "type" = "list"
-  default = [
-    {
-      name = "John Smith"
-      email = "john@smith.org"
-    },
-    {
-      name = "Jane Smith"
-      email = "jane@smith.org"
-    }
-  ]
+variable "alert_short_name" {
+  type = "string"
 }
 
-variable "alert_sms_receivers" {
-  "type" = "list"
-  default = []
-  description = <<EOF
-example
-[
-  {
-    name = "John Smith"
-    countryCode = "0044"
-    phoneNumber = "7777777777"
-  },
-  {
-    name = "Jane Smith"
-    countryCode = "0044"
-    phoneNumber = "7777777778"
-  }
-EOF
+variable "email_receiver_name" {
+  type = "string"
 }
 
-variable "alert_webhook_receivers" {
-  "type" = "list"
-  default = []
-  description = <<EOF
-example
-[
-  {
-    name = "Slack"
-    serviceUri = "https://slack.com/webhook"
-  },
-  {
-    name = "Something else"
-    serviceUri = "https://somethingelse.org/webhook"
-  }
-EOF
+variable "email_receiver_address" {
+  type = "string"
 }
