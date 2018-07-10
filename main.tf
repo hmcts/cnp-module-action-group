fb4bace4c144cf47217cd141cfd71986e0004b04
andrewwa-kainos <andrewwa@kainos.com>
2018-07-10 15:51:19 +0100

    Renamed variables and updated README

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ data "template_file" "actiongrouptemplate" {
 
 resource "azurerm_template_deployment" "action-group" {
   template_body       = "${data.template_file.actiongrouptemplate.rendered}"
-  name                = "${var.group_name}"
+  name                = "${var.action_group_name}"
   resource_group_name = "${var.resourcegroup_name}"
   deployment_mode     = "Incremental"
 
   parameters = {
     location                           = "${var.location}"
-    alertName                          = "${var.alert_name}"
-    alertShortName                     = "${var.alert_short_name}"
+    actionGroupName                    = "${var.action_group_name}"
+    shortName                          = "${var.short_name}"
     emailReceiverName                  = "${var.email_receiver_name}"
     emailReceiverAddress               = "${var.email_receiver_address}"
   }
