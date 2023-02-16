79b2333ffbb14d4be8caf703083ac6a2438565ba
damongreen123 <damon.green@btinternet.com>
2023-02-16 15:03:55 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "template_file" "actiongrouptemplate" {
 }
 
 resource "azurerm_resource_group_template_deployment" "action-group" {
-  template_content       = data.template_file.actiongrouptemplate.rendered
+  template_content    = data.template_file.actiongrouptemplate.rendered
   name                = var.short_name_var.env
   resource_group_name = var.resourcegroup_name
   deployment_mode     = "Incremental"
@@ -14,8 +14,7 @@ resource "azurerm_resource_group_template_deployment" "action-group" {
     shortName            = { value = var.short_name }
     emailReceiverName    = { value = var.email_receiver_name }
     emailReceiverAddress = { value = var.email_receiver_address }
-    commonTags           = { value = base64encode(jsonencode(var.common_tags))}
-  }
+  })
 }
 
 output "action_group_name" {
