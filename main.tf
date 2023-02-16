8809613601691d8794fbd919bcfef1392c4b5aed
damongreen123 <damon.green@btinternet.com>
2023-02-16 13:54:41 +0000

    New version of update-template branch

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 data "template_file" "actiongrouptemplate" {
-  template = "${file("${path.module}/templates/action-group.json")}"
+  template = file("${path.module}/templates/action-group.json")
 }
 
-resource "azurerm_template_deployment" "action-group" {
-  template_body       = "${data.template_file.actiongrouptemplate.rendered}"
-  name                = "${var.short_name}_${var.env}"
-  resource_group_name = "${var.resourcegroup_name}"
+resource "azurerm_resource_group_template_deployment" "action-group" {
+  template_content       = data.template_file.actiongrouptemplate.rendered
+  name                = var.short_name_var.env
+  resource_group_name = var.resourcegroup_name
   deployment_mode     = "Incremental"
 
-  parameters = {
-    location             = "${var.location}"
-    actionGroupName      = "${var.action_group_name}"
-    shortName            = "${var.short_name}"
-    emailReceiverName    = "${var.email_receiver_name}"
-    emailReceiverAddress = "${var.email_receiver_address}"
+  parameters_content = jsonencode({
+    location             = var.location
+    actionGroupName      = var.action_group_name
+    shortName            = var.short_name
+    emailReceiverName    = var.email_receiver_name
+    emailReceiverAddress = var.email_receiver_address
   }
 }
 
 output "action_group_name" {
-  value = "${var.action_group_name}"
+  value = var.action_group_name
 }
