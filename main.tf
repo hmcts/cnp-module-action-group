17422e0e110ba29180a6666df46909d2297661a6
damongreen123 <damon.green@btinternet.com>
2023-02-23 15:05:07 +0000

    Revert "New version of update-template branch"

--- main.tf
+++ main.tf
@@ -1,22 +1,22 @@
 data "template_file" "actiongrouptemplate" {
-  template = file("${path.module}/templates/action-group.json")
+  template = "${file("${path.module}/templates/action-group.json")}"
 }
 
-resource "azurerm_resource_group_template_deployment" "action-group" {
-  template_content    = data.template_file.actiongrouptemplate.rendered
-  name                = var.short_name
-  resource_group_name = var.resourcegroup_name
+resource "azurerm_template_deployment" "action-group" {
+  template_body       = "${data.template_file.actiongrouptemplate.rendered}"
+  name                = "${var.short_name}_${var.env}"
+  resource_group_name = "${var.resourcegroup_name}"
   deployment_mode     = "Incremental"
 
-  parameters_content = jsonencode({
-    location             = { value = var.location }
-    actionGroupName      = { value = var.action_group_name }
-    shortName            = { value = var.short_name }
-    emailReceiverName    = { value = var.email_receiver_name }
-    emailReceiverAddress = { value = var.email_receiver_address }
-  })
+  parameters = {
+    location             = "${var.location}"
+    actionGroupName      = "${var.action_group_name}"
+    shortName            = "${var.short_name}"
+    emailReceiverName    = "${var.email_receiver_name}"
+    emailReceiverAddress = "${var.email_receiver_address}"
+  }
 }
 
 output "action_group_name" {
-  value = var.action_group_name
+  value = "${var.action_group_name}"
 }
